8b1f3a18aaa00a680bfd6d8eb81947e890d2a506
Jaedee Delfin <binarykorra@icloud.com>
2022-08-17 01:59:06 +0800

    0

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-require('dotenv').config();
 const Mustache = require('mustache');
-const fetch = require('node-fetch');
 const fs = require('fs');
-
 const MUSTACHE_MAIN_DIR = './main.mustache';
-
+/**
+  * DATA is the object that contains all
+  * the data to be provided to Mustache
+  * Notice the "name" and "date" property.
+*/
 let DATA = {
-  refresh_date: new Date().toLocaleDateString('en-GB', {
+  name: 'Jaede Sy',
+  date: new Date().toLocaleDateString('en-GB', {
     weekday: 'long',
     month: 'long',
     day: 'numeric',
@@ -16,20 +18,16 @@ let DATA = {
     timeZone: 'Asia/Manila',
   }),
 };
-
-async function generateReadMe() {
-  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+/**
+  * A - We open 'main.mustache'
+  * B - We ask Mustache to render our file with the data
+  * C - We create a README.md file with the generated output
+  */
+function generateReadMe() {
+  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
     if (err) throw err;
     const output = Mustache.render(data.toString(), DATA);
     fs.writeFileSync('README.md', output);
   });
 }
-
-async function action() {
-  /**
-   * Generate README
-   */
-  await generateReadMe();
-}
-
-action();
+generateReadMe();
