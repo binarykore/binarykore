3ed4369fc7ce87ec1716cfe0189abf48dce28d7f
Jaedee Delfin <binarykorra@icloud.com>
2022-08-17 04:15:23 +0800

    0

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ let DATA = {
 
 async function setInstagramPosts() {
   const instagramImages = await puppeteerService.getLatestInstagramPostsFromAccount('binarymako.ph', 6);
-  DATA.img1 = instagramImages[1];
-  DATA.img2 = instagramImages[2];
-  DATA.img3 = instagramImages[3];
-  DATA.img4 = instagramImages[4];
-  DATA.img5 = instagramImages[5];
-  DATA.img6 = instagramImages[6];
+  DATA.img1 = instagramImages[0];
+  DATA.img2 = instagramImages[1];
+  DATA.img3 = instagramImages[2];
+  DATA.img4 = instagramImages[3];
+  DATA.img5 = instagramImages[4];
+  DATA.img6 = instagramImages[5];
 }
 
 async function generateReadMe() {
