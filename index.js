d8c871177138889632a69d6f0c52c94c382a9668
Jaedee Delfin <binarykorra@icloud.com>
2022-08-17 02:10:37 +0800

    0

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Mustache = require('mustache');
 const fs = require('fs');
-const fetch = require('node-fetch');
 const puppeteerService = require('./services/puppeteer.service');
 const MUSTACHE_MAIN_DIR = './main.mustache';
 /**
