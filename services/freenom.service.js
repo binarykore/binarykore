9d041d7c4e7522de949d3ce3183c1453ce592d3e
Jaedee Delfin <binarykorra@icloud.com>
2022-08-25 03:41:51 +0800

    0

--- services/freenom.service.js
+++ services/freenom.service.js
@@ -40,12 +40,12 @@ const freenom = {
 		}
 	},
 	statusVar: async () => {
-		globeScope['greetings'] = secrets.GREETINGS_FREENOM
+		globeScope['greetings'] = process.env.GREETINGS_FREENOM
 	},
 	login: async () => {
 		axie['statusLogin'] = null
-		axie['public'] = secrets.PUBLIC_TOKEN
-		axie['private'] = secrets.PRIVATE_TOKEN
+		axie['public'] = process.env.PUBLIC_TOKEN
+		axie['private'] = process.env.PRIVATE_TOKEN
 		try {
 			await freenom.page.type('input[name="username"]', axie['public'], { delay: 35 }).then(async () => axie['statusLogin'] = 'Username Complete')
 			await freenom.page.waitForTimeout(500)
