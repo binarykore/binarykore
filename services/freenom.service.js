6fcdece7933663ba607787e649b62ea3f19388ed
Jaedee Delfin <binarykorra@icloud.com>
2022-08-25 04:32:33 +0800

    0

--- services/freenom.service.js
+++ services/freenom.service.js
@@ -14,14 +14,16 @@ const freenom = {
 	init: async (public_token,private_token) => {
 		freenom.browser = await puppeteer.launch({
 		  args: [
+			'--disable-gpu',
 			'--no-sandbox',
 			'--disable-setuid-sandbox',
+			'--disable-dev-shm-usage',
+			'--no-first-run',
+			'--no-zygote',
 			'--disable-infobars',
 			'--window-position=0,0',
 			'--ignore-certifcate-errors',
 			'--ignore-certifcate-errors-spki-list',
-			'--incognito',
-			'--proxy-server=http=194.67.37.90:3128',
 			// '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"', //
 		  ],
 		  // headless: false,
