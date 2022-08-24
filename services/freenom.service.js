b1eeb561a7e497cd047bb77d450dc45ff8fb3d2c
Jaedee Delfin <binarykorra@icloud.com>
2022-08-25 03:34:59 +0800

    0

--- services/freenom.service.js
+++ services/freenom.service.js
@@ -40,7 +40,7 @@ const freenom = {
 		}
 	},
 	statusVar: async () => {
-		globeScope['greetings'] = process.env.GREETINGS_FREENOM
+		globeScope['greetings'] = secrets.GREETINGS_FREENOM
 	},
 	login: async () => {
 		axie['statusLogin'] = null
