fe161eb1070987e4399cf3d1f7f4fbbf7bd0c919
Jaedee Delfin <binarykorra@icloud.com>
2022-08-25 04:34:41 +0800

    0

--- services/freenom.service.js
+++ services/freenom.service.js
@@ -13,20 +13,21 @@ const freenom = {
 	},
 	init: async (public_token,private_token) => {
 		freenom.browser = await puppeteer.launch({
-		  args: [
-			'--disable-gpu',
-			'--no-sandbox',
-			'--disable-setuid-sandbox',
-			'--disable-dev-shm-usage',
-			'--no-first-run',
-			'--no-zygote',
-			'--disable-infobars',
-			'--window-position=0,0',
-			'--ignore-certifcate-errors',
-			'--ignore-certifcate-errors-spki-list',
-			// '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"', //
-		  ],
-		  // headless: false,
+			headless: true,
+			ignoreDefaultArgs: ['--disable-extensions'],
+			args: [
+				'--disable-gpu',
+				'--no-sandbox',
+				'--disable-setuid-sandbox',
+				'--disable-dev-shm-usage',
+				'--no-first-run',
+				'--no-zygote',
+				'--disable-infobars',
+				'--window-position=0,0',
+				'--ignore-certifcate-errors',
+				'--ignore-certifcate-errors-spki-list',
+				// '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"', //
+			],
 		});
 		try {
 			freenom.page = await freenom.browser.newPage()
