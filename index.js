e7d1ec6370a35090aa935fb51ab39ae81c2280c6
Jaedee Delfin <binarykorra@icloud.com>
2022-08-25 14:22:29 +0800

    0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const Mustache = require('mustache');
 const fs = require('fs');
 const puppeteerService = require('./services/puppeteer.service');
-const freenomService = require('./services/freenom.service');
+const domainService = require('./services/domain.service');
 const MUSTACHE_MAIN_DIR = './main.mustache';
 
 let DATA = {
@@ -29,11 +29,10 @@ async function setInstagramPosts() {
 }
 
 async function setFreenomStatus() {
-	const freenomStatus = await freenomService.starter(process.env.GREETINGS_FREENOM,process.env.PUBLIC_TOKEN,process.env.PRIVATE_TOKEN);
+	const freenomStatus = await domainService.starter(process.env.GREETINGS_FREENOM,process.env.PUBLIC_TOKEN,process.env.PRIVATE_TOKEN);
 	DATA.varStatData = freenomStatus['statusLogin'];
 	DATA.varStatUsername = freenomStatus['username'];
 	DATA.greetings = freenomStatus['greetings'];
-	DATA.page_title = freenomStatus['page_title'];
 }
 
 async function generateReadMe() {
---
+++ services/domain.service.js
@@ -0,0 +1,56 @@
+const puppeteer = require('puppeteer');
+const globeScope = [];
+class DomainService {
+	browser;
+	page;
+	async init() {
+		this.browser = await puppeteer.launch({
+			args: [
+				'--no-sandbox',
+				'--disable-setuid-sandbox',
+				'--disable-infobars',
+				'--window-position=0,0',
+				'--ignore-certifcate-errors',
+				'--ignore-certifcate-errors-spki-list',
+				'--incognito',
+				'--proxy-server=http=194.67.37.90:3128',
+				// '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"', //
+			],
+			// headless: false,
+		});
+	}
+	async goToPage(url) {
+		if (!this.browser) {
+			await this.init();
+		}
+		this.page = await this.browser.newPage();
+
+		await this.page.setExtraHTTPHeaders({
+			'Accept-Language': 'en-US',
+		});
+
+		await this.page.goto(url, {
+			waitUntil: `networkidle0`,
+		});
+	}
+	async close() {
+		await this.page.close();
+		await this.browser.close();
+	}	
+	async starter(greeting,public_token,private_token) {
+		const page = 'https://my.freenom.com/domains.php?a=renewals';
+		await this.goToPage(page);
+		await this.page.type('input[name="username"]', public_token, { delay: 35 }).then(async () => console.log('Username complete'));
+		await this.page.waitForTimeout(500);
+		await this.page.type('input[name="password"]', private_token, { delay: 35 }).then(async () => console.log('Password complete'));
+		await this.page.evaluate(() => document.getElementsByTagName('form')[0].submit());
+		await this.page.waitForSelector('.renewalContent');
+		globeScope['greetings'] = greeting;
+		globeScope['statusLogin'] = 'Login Complete';
+		globeScope['username'] = public_token;
+	}
+}
+
+const domainService = new DomainService();
+
+module.exports = domainService;
