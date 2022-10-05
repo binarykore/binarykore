b423f29eb81765c3851bbd1ffaa31c95d65fb8c5
Jaedee Delfin <binarykorra@icloud.com>
2022-10-06 01:58:12 +0800

    0

--- services/puppeteer.service.js
+++ services/puppeteer.service.js
@@ -85,9 +85,9 @@ class PuppeteerService {
       // await this.page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
       await this.page.waitFor(1000);
 
-      const nodes = await this.page.evaluate(() => {
-        const forexCount = document.querySelectorAll(`.tv-symbol-header__first-line`);
-        return [].map.call(forexCount, div => div.innerHTML);
+      const nodes = await this.page.waitForSelector(`.js-symbol-last`).evaluate(() => {
+        const forexCount = document.querySelectorAll(`.js-symbol-last`);
+        return [].map.call(forexCount, div => div.textContent);
       });
 
       return nodes.slice(0, 2);
