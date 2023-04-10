e6fc0049dcc02b6c31c345b3f7a3cfcbb47814db
Jose Mari Delfin <binarykorra@icloud.com>
2023-04-10 17:46:51 +0800

    0

--- services/axios.service.js
+++ services/axios.service.js
@@ -4,14 +4,14 @@ function axie(item, index){
 	data[index] = item;
 }
 class axiePromise{
-	async parseData(response) {
+	async parseData(response){
 		response.forEach(axie);
 		return JSON.parse(data);
 	}
 	async getForexUpdates(url, n) {
 		try {
 			axios.get(url).then(function(response){
-				const nodes = await this.parseData(response);
+				const nodes = this.parseData(response);
 				console.log(nodes);
 			}).catch(function (error){
 				console.log(error);
